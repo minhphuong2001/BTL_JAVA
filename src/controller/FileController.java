c75b6cfad8384e486cfac6005033f106251a30ee
minhphuong2001 <minhphuongdo2001@gmail.com>
2022-05-06 14:39:19 +0700

    setup

---
+++ src/controller/FileController.java
@@ -0,0 +1,61 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import modal.Account;
+
+/**
+ *
+ * @author Minh Phuong Do
+ */
+public class FileController {
+    static FileWriter fileWriter;
+    static BufferedWriter bufWriter;
+    static FileReader fileReader;
+    static BufferedReader bufReader;
+
+    public static void writeAccountToFile(String filename, Account account){
+        try {
+            fileWriter = new FileWriter(filename, true);
+            bufWriter = new BufferedWriter(fileWriter);
+            bufWriter.write(account.toFile());
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            try {
+                bufWriter.close();  
+                fileWriter.close();
+            } catch (Exception e) {         
+                System.out.println(e);  
+            }
+        }
+    }
+
+    public static List<Account> readAccountToFile(String filename){
+        List<Account> listAcc = new ArrayList<Account>();
+        try {
+            fileReader = new FileReader(filename);
+            bufReader = new BufferedReader(fileReader);
+            String line = "";
+            while((line = bufReader.readLine()) != null){
+                String data[] = line.toString().split("\\|");
+                Account account = new Account(data[0], data[1],data[2], data[3]);
+                listAcc.add(account);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            try {
+                bufWriter.close();  
+                fileWriter.close();
+            } catch (Exception e) {         
+                System.out.println(e);  
+            }
+        }
+        return listAcc;
+    }
+}
---
+++ src/modal/Account.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modal;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Account {
+    private String id;
+    private String username;
+    private String password;
+    private String role;
+
+    public String toFile(){
+        return String.format("%s\\|%s\\|%s\\|%s%n", id, username, password, role);
+    }
+
+    public Account(){}
+
+    public Account(String id, String username, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}

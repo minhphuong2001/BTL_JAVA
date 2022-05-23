1807aca9354f77f54f77561da5f154c222e9161f
nhl2610 <nhluyen1026@gmail.com>
2022-05-23 23:25:17 +0700

    luyen add QLKH

--- src/modal/Customer.java
+++ src/modal/Customer.java
@@ -6,8 +6,56 @@ package modal;
 
 /**
  *
- * @author ADMIN
+ * @author Nguyen Hai Luyen
  */
 public class Customer {
+    private Integer customerId;
+    private String customerName;
+    private String customerPhone;
+    private Float accumulatePoints;
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public Float getAccumulatePoints() {
+        return accumulatePoints;
+    }
+
+    public void setAccumulatePoints(Float accumulatePoints) {
+        this.accumulatePoints = accumulatePoints;
+    }
+
+    public Customer() {
+    }
+    
+
+    public Customer(Integer customerId, String customerName, String customerPhone, Float accumulatePoints) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerPhone = customerPhone;
+        this.accumulatePoints = accumulatePoints;
+    }
+    
     
 }
